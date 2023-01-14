3105ec2cfbfb64de76506dded633ddda64553f70
Snehasis Ghosh <91667746+snehasisserver@users.noreply.github.com>
2023-01-14 15:24:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function preload(){}
+function setup(){
+ Canvas = createCanvas(600,600);
+ Canvas.center();
+ Canvas.background("white");     
+}
+function draw(){
+    fill("red");
+   rect(200,200,30,30);
+}
